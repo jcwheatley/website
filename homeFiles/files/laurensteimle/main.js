5c874654f8b0ef47c91d4696d310f50907162342
James Wheatley <jcwheatley1@gmail.com>
2018-10-18 02:21:48 -0400

    finished mobile nav, hooked up firebase contact form, other minor cosmetic fixes

---
+++ homeFiles/files/laurensteimle/main.js
@@ -0,0 +1,66 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDxx3K-Cb219RablzgMFveUzs-fkCU6ZAc",
+    authDomain: "contact-form-66589.firebaseapp.com",
+    databaseURL: "https://contact-form-66589.firebaseio.com",
+    projectId: "contact-form-66589",
+    storageBucket: "",
+    messagingSenderId: "80276289504"
+  };
+  firebase.initializeApp(config);
+
+// // Initialize Firebase James' Test Database
+// var config = {
+//     apiKey: "AIzaSyA6hLUTjFdo9ZlYse6BivNk_7Asco5I9Po",
+//     authDomain: "ls-contactform.firebaseapp.com",
+//     databaseURL: "https://ls-contactform.firebaseio.com",
+//     projectId: "ls-contactform",
+//     storageBucket: "",
+//     messagingSenderId: "992758537534"
+// };
+// firebase.initializeApp(config);
+
+// Contact Form Submissions Collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for submit pushed
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+
+function submitForm(e){
+    e.preventDefault();
+
+    var name = getInputValue('name');
+    var email = getInputValue('email');
+    var subject = getInputValue('subject');
+    var message = getInputValue('message');
+
+    saveMessage(name, email, subject, message);
+
+    // Message sent confirmation alert
+    document.querySelector('.alert').style.display = 'block';
+
+    //Hide alert after 10 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none';
+    },5000);
+
+    //Clear all forms
+    document.getElementById('contactForm').reset();
+}
+
+// get form values
+function getInputValue(id){
+    return document.getElementById(id).value;
+}
+
+// Save messages to firebase
+function saveMessage(name, email, subject, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    });
+}
